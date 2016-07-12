9941709a7db82d06f8218a7d9fc417d5a161c0e9
heroming <heroming7788@gmail.com>
2016-07-12 20:05:38 +0800

    Add vim config files

---
+++ vim/_run_java.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: momodi
+ * Created Time:  2010/2/12 13:42:48
+ * File Name: run.cpp
+ * Description: 
+ */
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+const int maxn = 256;
+
+int main(int argc, char **argv) {
+    char compiler[maxn] = {};
+    sprintf(compiler, "javac %s.java", argv[1], argv[1]);
+    char runcommand[maxn] = {};
+    sprintf(runcommand, "java %s", argv[1]);
+    if (system(compiler) == 0) {
+        int t = clock();
+        if (system(runcommand) == 0) {
+            printf("\nRun Time: %dms\n", clock() - t);
+        }
+    }
+    return 0;
+}
+
