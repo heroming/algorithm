9ed2efe05e59e419b546ef3ceb9ceea94f77e69b
minghao <heroming7788@gmail.com>
2017-01-05 21:06:17 +0800

    leetcode practice

---
+++ leetcode/371_Sum_of_Two_Integers.cpp
@@ -0,0 +1,72 @@
+/*
+ * Author:heroming
+ * File:heroming.cpp
+ * Time:2017/01/05 03:01:18
+ */
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <unordered_map>
+using namespace std;
+
+#define px first
+#define py second
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+#define clr(v, e) memset(v, e, sizeof(v))
+#define rep(it, v) for (auto it : v)
+#define forn(i, n) for (int i = 0; i < (n); ++ i)
+#define rforn(i, n) for (int i = (n) - 1; i >= 0; -- i)
+#define form(i, a, b) for (int i = (a); i <= (b); ++ i)
+#define rform(i, a, b) for (int i = (b); i >= (a); -- i)
+#define forv(i, v) for (int i = 0; i < sz(v); ++ i)
+#define iter(it, v) for (auto it = v.begin(); it != v.end(); ++ it)
+
+typedef long long lint;
+typedef vector<int> vint;
+typedef vector<string> vstring;
+typedef pair<int, int> pint;
+typedef vector<lint> vlint;
+typedef vector<pint> vpint;
+
+class Solution {
+public:
+
+    int getSum(int a, int b) {
+        int ret = a;
+        while (b) {
+            ret = a ^ b;
+            b = (a & b) << 1;
+            a = ret;
+        }
+        return ret;
+    }
+};
+
+int main() {
+    Solution so;
+    int a, b;
+    while (cin >> a >> b) {
+        printf("%d + %d = %d\n", a, b, so.getSum(a, b));
+    }
+    return 0;
+}
